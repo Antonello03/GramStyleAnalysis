983a6600d7204d6f8cff5a8c1543bc411a28cf8c
Antonello03 <antoplay03@gmail.com>
2025-09-11 21:48:07 +0200

    modified generator!

--- final_experiment/generator.py
+++ final_experiment/generator.py
@@ -12,7 +12,7 @@ from model import VGG
 from losses import GramMSELoss, PearsonCorrelationLoss, CosineSimilarityLoss
 from config import compute_ratio_style_weights, style_layers, content_layers, max_iter_alt as max_iter, lr_alt as lr # Speed Up
 from preprocessing import prep
-from engine import synthesizeImage
+from engine import synthesizeImage, synthesizeImage3
 
 # Parameters for reproducibility
 seed = 42
@@ -37,7 +37,7 @@ if torch.cuda.is_available():
 # Content and Style Images, Output Directory
 content_dir = 'final_experiment/content/'
 style_dir = 'final_experiment/style/'
-output_dir = 'final_experiment/output_alt'
+output_dir = 'final_experiment/output_alt_newlr'
 
 
 content_imgs = [Image.open(os.path.join(content_dir, f)) for f in os.listdir(content_dir) if f.endswith('.jpg')]
@@ -49,7 +49,8 @@ content_names = [os.path.splitext(f)[0] for f in os.listdir(content_dir) if f.en
 style_names = [os.path.splitext(f)[0] for f in os.listdir(style_dir) if f.endswith('.jpg')]
 
 i = 0
-style_loss_weight = 7
+threshold = 0.5
+style_loss_weight = 6.5
 TOTAL_SAMPLES = len(content_imgs) * len(style_imgs)
 start = time.perf_counter()
 
@@ -60,9 +61,9 @@ def fmt_time(sec):
     return f"{h:d}:{m:02d}:{s:02d}" if h else f"{m:02d}:{s:02d}"
 
 losses_list = [
-   ("rmse", GramMSELoss),
-   ("prs",  PearsonCorrelationLoss),
-   ("cos",  CosineSimilarityLoss)
+   ("rmse", GramMSELoss, 1.5),
+   ("prs",  PearsonCorrelationLoss, 1.5),
+   ("cos",  CosineSimilarityLoss, 1.75)
 ]
 
 for idx_c, content_img in enumerate(content_imgs):
@@ -70,11 +71,11 @@ for idx_c, content_img in enumerate(content_imgs):
         i += 1
         c_name, s_name = content_names[idx_c], style_names[idx_s]
 
-        for suffix, loss_cls in losses_list:
+        for suffix, loss_cls, lr in losses_list:
             sw, cw = compute_ratio_style_weights(vgg, content_img, style_img, style_layers, content_layers, loss_cls, style_loss_weight=style_loss_weight, verbose=False, individual_layer_weights_by_loss= True) # different method
-            synthesizeImage(
-                vgg, style_img, content_img, loss_cls, sw, cw,
-                max_iter=max_iter, show_iter=max_iter*3, lr= lr
+            synthesizeImage3(
+                style_img, content_img, loss_cls, sw, cw,
+                max_iter=max_iter, show_iter=max_iter*3, threshold=threshold, lr=lr
             ).save(f"{output_dir}/{s_name}_{c_name}_{suffix}.jpg")
 
         elapsed = time.perf_counter() - start
